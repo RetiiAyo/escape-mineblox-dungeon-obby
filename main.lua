16cecb40739f8922d36522f152680cc5c897ce6e
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-02-21 19:34:04 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,43 @@
+local Folder = Instance.new("Folder", game.Workspace)
+Folder.Name = "waypoints_000"
+local part = Instance.new("Part", game.Workspace)
+part.Name = "waypoint_000"
+
+function getPath()
+    local Part
+    Part = game.Workspace:FindFirstChild("waypoint_000")
+    return Part
+end
+
+while true do
+
+    local Destination = getPath()
+
+    local path = PathfindingService:CreatePath({ WaypointSpacing = 1, AgentRadius = 0.1, AgentCanJump = true })
+    path:ComputeAsync(LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0,3,0), Destination.Position)
+    local Waypoints = path:GetWaypoints()
+
+    if path.Status ~= Enum.PathStatus.NoPath then
+
+        Folder:ClearAllChildren()
+
+        for _, Waypoint in pairs(Waypoints) do
+            local part = Instance.new("Part")
+            part.Size = Vector3.new(1,1,1)
+            part.Position = Waypoint.Position
+            part.Shape = "Cylinder"
+            part.Rotation = Vector3.new(0,0,90)
+            part.Material = "SmoothPlastic"
+            part.Anchored = true
+            part.CanCollide = false
+            part.Parent = Folder
+        end
+
+        for _, Waypoint in pairs(Waypoints) do
+            if LocalPlayer.Character.HumanoidRootPart.Anchored == false then
+                LocalPlayer.Character.Humanoid:MoveTo(Waypoint.Position)
+                LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
+            end
+        end
+    end
+end
