04bbafef9ed2cd62a91ba98217ff51a0ab0bf068
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-02-21 19:40:59 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ while true do
 
     local Destination = getPath()
     local PathfindingService = game:getService("PathfindingService")
+    local LocalPlayer = game.Players.LocalPlayer
 
     local path = PathfindingService:CreatePath({ WaypointSpacing = 1, AgentRadius = 0.1, AgentCanJump = true })
     path:ComputeAsync(LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0,3,0), Destination.Position)
